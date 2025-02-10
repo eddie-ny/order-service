b68db2b2967332f967807ba9727c6711980ea985
Edward Ayirebi Acquah <129085304+eddie-ny@users.noreply.github.com>
2025-02-10 11:06:11 +0000

    Delete order-service directory

--- order-service/src/utils/generateOrderId.js
+++
@@ -1,8 +0,0 @@
-// generateOrderId.js
-const generateOrderId = () => {
-    const timestamp = Date.now();
-    const randomString = Math.random().toString(36).substring(2, 15);
-    return `${timestamp}-${randomString}`;
-};
-
-module.exports = generateOrderId;
